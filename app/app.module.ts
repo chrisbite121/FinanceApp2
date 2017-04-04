6d3037fe3dacf32b8e8199316d7f4112432c4698
chris ercilla <chris ercilla>
2017-04-04 09:15:55 +0700

    ag-grid quickstart

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+// ag-grid
+import {AgGridModule} from "ag-grid-angular/main";
+// application
+import {AppComponent} from "./app.component";
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        AgGridModule.withComponents([]
+        )
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ app/vendor-aot.ts
@@ -0,0 +1,16 @@
+// app/vendor-aot.ts 
+// Angular
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+import '@angular/forms';
+
+// RxJS
+import 'rxjs';
+
+// ag-grid
+import 'ag-grid/dist/styles/ag-grid.css';
+import 'ag-grid/dist/styles/theme-fresh.css';
+import 'ag-grid-angular/main'
